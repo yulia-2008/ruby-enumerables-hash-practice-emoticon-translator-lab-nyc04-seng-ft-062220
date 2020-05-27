1f067656c0df0f1f491874e5ab4fd4db26f6c202
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-27 11:04:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,24 @@
-# require modules here
+#require modules here 
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file)
+  emoticon=YAML.load_file(file)
+  hasj={}
+  
+  
+ new_array=[]
+  hash={}
+   array=emoticon.split('| |')
+  array.each do |ind|
+    new_array << ind.split('|')
+    new_array.each do |index|
+        hash[index[0]]={:english => index[1], :japanese => index[2]}
+
+    
+   hash
 end
+
 
 def get_japanese_emoticon
   # code goes here
