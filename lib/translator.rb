7cff241e162e78c1702d49c5f873130ee8134819
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-27 13:07:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,10 +8,15 @@ def load_library(file)
   library.each do |key, value|
   hash[key]={:english => value[0], :japanese => value[1]}
 end 
-
+hash.each do |key, value|
+    value.each do |key1, value1|
+    if value1 == emoticon 
+      return key 
+    else 
+      return "Sorry, that emoticon was not found"
 
 end  
-#load_library('./lib/emoticons.yml')  
+load_library('./lib/emoticons.yml')  
   
 
 
